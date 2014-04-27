34fd8d9da19d34437dde1a652a3cb903c3cfcc28
Jakob <jakob.grannas@intelliplan.se>
2014-04-27 22:30:09 +0200

    Moved some code to services and refactored controller code

--- api/controllers/RecipeController.js
+++ api/controllers/RecipeController.js
@@ -15,97 +15,6 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
-var _phantom = require('phantom'),
-    _ = require('underscore'),
-    Q = require('q');
-
-function scanPhantomPage(url) {
-	var deferred = Q.defer();
-	_phantom.create("--web-security=no", "--ignore-ssl-errors=yes", { port: 12345 }, function (ph) {
-		ph.createPage(function(page) {
-			page.open(url, function (status) {
-				if (status == "success") {
-					deferred.resolve(page);
-				}
-				else {
-					deferred.reject('Could not open page');
-				}
-			});
-		});
-	});
-
-	return deferred.promise;
-}
-
-function getReceptNuRecipe (page) {
-	var deferred = Q.defer();
-	page.evaluate(
-		function (selector) {
-			// TOOO: How to execute these in right scope?
-			function getInstructions (instructions) {
-				var result = [];
-
-				for (var i=0; i < instructions.length; i++) {
-					result.push(instructions[i].innerText.trim());
-				}
-
-				return result;
-			}
-
-			// TODO: Move to receptnu class or w/e
-			function getIngredients (ingredients) {
-				var result = [], ingredient;
-
-				for (var i=0; i < ingredients.length; i++) {
-					ingredient = ingredients[i];
-					result.push({
-						amount: ingredient.innerText.trim(),
-						name: ingredient.nextElementSibling.innerText.trim()
-					});
-				}
-				return result;
-			}
-
-			var ingredients = document.querySelectorAll('#ingredients .ingredient'),
-				instructions = document.querySelectorAll('.step-by-step li > span');
-			var resultObj = {
-					title: document.querySelector('.basic-info h1').firstChild.textContent.trim(),
-					cookingTime: document.querySelector('.basic-info .time').innerText.trim(),
-					amountOfPersons: document.querySelector('.basic-info .amount').innerText.trim(),
-					instructions: getInstructions(instructions),
-					ingredients: getIngredients(ingredients)
-				};
-
-			return resultObj;
-		},
-		function (result) {
-			//this log will be printed in the Node console
-			console.log("Result: ", result);
-			deferred.resolve(result);
-		}
-	);
-	page.close();
-	return deferred.promise;
-}
-
-function getRecipe (url) {
-	var getterFn;
-	if (url.indexOf('recept.nu') > -1) {
-		getterFn = getReceptNuRecipe;
-	}
-	else if(url.indexOf('coop.se') > -1) {
-		getterFn = getCoopRecipe;
-	}
-	else if(url.indexOf('alltommat.se') > -1) {
-		getterFn = getAlltOmMatRecipe;
-	}
-
-	if(getterFn) {
-		return scanPhantomPage(url)
-			.then(getterFn);
-	}
-}
-
 module.exports = {
 
 	/**
@@ -114,11 +23,17 @@ module.exports = {
 	 * @param response
 	 */
 	import: function (request, response) {
-		getRecipe("http://www.recept.nu/paolo-roberto/varmratter/fisk-och-skaldjur/pasta-med-vitloksfrasta-rakor-och-vitt-vin/") // Test url
-			//.then()
-		.then(function (result) {
-			response.send(result);
-		});
+		RecipeService.getRecipe("http://www.recept.nu/paolo-roberto/varmratter/fisk-och-skaldjur/pasta-med-vitloksfrasta-rakor-och-vitt-vin/") // Test url
+		.then(
+			function (result) {
+				response.send(result);
+			},
+			function (rejectReason) {
+				response.send({
+					errorMessage: rejectReason
+				});
+			}
+		);
 	},
 
 	/**
